b2c6790e2158d9fd29cb8212d86f58448f82fec4
Daricyos <59017694+Daricyos@users.noreply.github.com>
2021-04-16 16:53:25 +0300

    Подключение sqlite

--- main.py
+++ main.py
@@ -8,10 +8,20 @@ from requests import get
 bot = telebot.TeleBot(config.token)
 
 
+class User:
+    def __init__(self):
+        connect = sqlite3.connect('users.db')
+        cursor = connect.cursor()
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS login_id(
+            userid INTEGER
+            city TEXT
+        )""")
+
+    connect.commit()
 
 
 user_list = []
-
 
 
 @bot.message_handler(commands=['start'])
@@ -22,36 +32,17 @@ def start_bot(message):
 
 @bot.message_handler(content_types = ['text'])
 def keyboard(message):
-    connect = sqlite3.connect('users.db')
-    cursor = connect.cursor()
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS login_id(
-        userid INTEGER
-        city TEXT
-    )""")
-
-    connect.commit()
-    user_list = []
-
     if message.text == lg.btn4:
         markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
         itembtn1 = types.KeyboardButton('Киев')
         itembtn2 = types.KeyboardButton('Главное меню⚡')
         markup.add(itembtn1, itembtn2)
-        user_id = message.chat.id
-        user_list.append(user_id)
 
         msg = bot.send_message(message.chat.id, '*Ваш город?*', reply_markup=markup, parse_mode="Markdown")
         bot.register_next_step_handler(msg, process_city_step)
 
 def process_city_step(message):
     chat_id = message.chat.id
-    user_list.append(chat_id)
-    keyboard.cursor.execute("INSERT INTO login_id VALUES(?,?);", user_list)
-    keyboard.connect.commit()
-
-
-
 
 
 
